568bd77aab08dd15006d0d3ca90e7a668c3c5b6f
enzomakenzy <enzomqueiroz@gmail.com>
2025-07-01 09:07:21 -0300

    Questão 2 do exercício 004 finalizada

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import styled from "styled-components/native";
 
 export default function App() {
+  const [ inputContent, setInputContent ] = useState("");
+  
   return (
     <Container>
       <Title>Minhas Tarefas</Title>
+      <Input 
+        value={inputContent}
+        onChangeText={setInputContent}
+      />
+      <InputTextResult>{inputContent}</InputTextResult>
     </Container>
   );
 }
@@ -12,10 +20,26 @@ const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: #e7e6e8;
+  background-color: #e6e6e6;
+  gap: 12px;
 `;
 
 const Title = styled.Text`
   font-size: 30px;
   font-weight: 700;
+  color: #58008f;
 `;
+
+const Input = styled.TextInput`
+  border-width: 1px;
+  border-color: #58008f;
+  padding: 8px;
+  width: 275px;
+  font-size: 16px;
+`;
+
+const InputTextResult = styled.Text`
+  font-size: 18px;
+  font-weight: 500;
+  color: #58008f;
+`
