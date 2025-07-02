300282e48d8ada2a82ff2bcb2f6b58197f29f59b
Enzo Makenzy <143420071+enzomakenzy@users.noreply.github.com>
2025-07-02 14:57:27 -0300

    Questão 6 do exercício 004 finalizada

--- App.tsx
+++ App.tsx
@@ -1,19 +1,27 @@
 import { useState } from "react";
-import { Alert, ScrollView } from "react-native";
-import AntDesign from '@expo/vector-icons/AntDesign';
-import styled from "styled-components/native";
+import { Alert, ScrollView, StatusBar } from "react-native";
+
+import { Container, Title, AddTaskContainer, Input, AddTaskButton, AddTaskIcon, TasklistTextHeader, TasklistEmptyText, TaskContainer, Task, IconTouch, RemoveTaskIcon, Loading } from "./styles/style"
+
+import { Quicksand_500Medium, Quicksand_700Bold, useFonts } from "@expo-google-fonts/quicksand"
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({ Quicksand_500Medium, Quicksand_700Bold });
+
   const [ inputContent, setInputContent ] = useState("");
   const [ taskList, setTaskList] = useState<string[]>([])
 
   const handleAddTask = () => {
+    setInputContent("")
     if (inputContent === "") {
-      Alert.alert("Item vazio", "A tarefa adicionada não pode ser vazia!");
-    } else {
-      setTaskList([...taskList, inputContent]);
-      setInputContent("")
+      return Alert.alert("Item vazio", "A tarefa adicionada não pode ser vazia!");
+    } 
+
+    if (inputContent.length > 48) {
+      return Alert.alert("A tarefa não pode ter mais de 48 caracteres");
     }
+
+    setTaskList([...taskList, inputContent]);
   }
 
   const handleRemoveTask = (task: string) => {
@@ -22,96 +30,50 @@ export default function App() {
   
   return (
     <Container>
-      <Title>Minhas Tarefas</Title>
-      <AddTaskContainer>
-        <Input 
-          value={inputContent}
-          onChangeText={setInputContent}
-        />
+      {
+        fontsLoaded ?  
+          <>
+            <StatusBar barStyle="light-content" />
+            <Title>Minhas Tarefas</Title>
+            <AddTaskContainer>
+              <Input 
+                value={inputContent}
+                onChangeText={setInputContent}
+              />
 
-        <AddTaskButton onPress={handleAddTask}>
-          <AddTaskTextButton>Adicionar Tarefa</AddTaskTextButton>
-        </AddTaskButton>
-      </AddTaskContainer>
+              <AddTaskButton onPress={handleAddTask}>
+                <AddTaskIcon name="add-circle" />
+              </AddTaskButton>
+            </AddTaskContainer>
 
-      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 40 }}>
-        {
-          taskList.map((task, index) => (
-            <TaskContainer key={index}>
-              <Task>{task}</Task>
-              <IconTouch onPress={() => handleRemoveTask(task)}>
-                <CloseIcon name="close" />
-              </IconTouch>
-            </TaskContainer>
-          ))
-        }
-      </ScrollView>
+            <ScrollView 
+              showsVerticalScrollIndicator={false} 
+              contentContainerStyle={{ paddingHorizontal: 8, paddingBottom: 40  }}>
+              {
+                taskList.length > 0 &&
+                <TasklistTextHeader>
+                  Tarefas
+                </TasklistTextHeader>
+              }
+
+              {
+                taskList.length === 0 ? 
+                <TasklistEmptyText>Você não possui tarefas {"\n"} no momento...</TasklistEmptyText>
+                :
+                taskList.map((task, index) => (
+                  <TaskContainer key={index}>
+                    <Task>{task}</Task>
+                    <IconTouch onPress={() => handleRemoveTask(task)}>
+                      <RemoveTaskIcon name="close" />
+                    </IconTouch>
+                  </TaskContainer>
+                ))
+              }
+            </ScrollView>
+          </>
+        : 
+          <Loading />
+      }
     </Container>
   );
 }
-
-const Container = styled.View`
-  flex: 1;
-  align-items: center;
-  background-color: #212121;
-  gap: 15px;
-  padding: 65px 20px 50px;
-`;
-
-const Title = styled.Text`
-  font-size: 30px;
-  font-weight: 700;
-  color: #7700ff;
-`;
-
-const AddTaskContainer = styled.View`
-  flex-direction: row;
-  gap: 15px;
-`;
-
-const Input = styled.TextInput`
-  border-width: 1px;
-  border-color: #e6e6e6;
-  border-radius: 5px;
-  padding: 8px;
-  flex: 1;
-  font-size: 16px;
-  color: #e6e6e6;
-`;
-
-const AddTaskButton = styled.TouchableOpacity.attrs({
-  activeOpacity: 0.6
-})`
-  background-color: #7700ff;
-  padding: 8px;
-  border-radius: 5px;
-`;
-
-const AddTaskTextButton = styled.Text`
-  color: #e6e6e6;
-  font-size: 16px;
-  font-weight: 600;
-`
-
-const TaskContainer = styled.View`
-  flex-direction: row;
-  width: 100%;
-  justify-content: space-between;
-  margin-top: 20px;
-  align-items: center;
-`
-
-const Task = styled.Text`
-  font-size: 18px;
-  font-weight: 500;
-  color: #e6e6e6;
-`
-
-const IconTouch = styled.TouchableOpacity.attrs({
-  activeOpacity: 0.6
-})``;
-
-const CloseIcon = styled(AntDesign).attrs({
-  color: "#e6e6e6",
-  size: 30,
-})``;
---
+++ styles/style.ts
@@ -0,0 +1,93 @@
+import styled from "styled-components/native";
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { ActivityIndicator } from "react-native";
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: #212121;
+  gap: 25px;
+  padding: 0px 20px 50px 20px;
+`;
+
+export const Title = styled.Text`
+  font-size: 30px;
+  font-family: "Quicksand_700Bold";
+  color: #8c00ff;
+  padding-top: 70px;
+`;
+
+export const AddTaskContainer = styled.View`
+  flex-direction: row;
+  gap: 15px;
+  align-items: center;
+`;
+
+export const Input = styled.TextInput`
+  border-width: 1px;
+  border-color: #e6e6e6;
+  border-radius: 50px;
+  height: 43px;
+  padding: 10px 20px;
+  flex: 1;
+  font-size: 16px;
+  font-family: "Quicksand_500Medium";
+  color: #e6e6e6;
+`;
+
+export const AddTaskButton = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.6
+})``;
+
+export const AddTaskIcon = styled(Ionicons).attrs({
+  color: "#8c00ff",
+  size: 48
+})``;
+
+export const TasklistEmptyText = styled.Text`
+  color: #e6e6e63b;
+  font-size: 18px;
+  font-family: "Quicksand_500Medium";
+  text-align: center;
+  line-height: 32px;
+  margin-top: 60px;
+`;
+
+export const TasklistTextHeader = styled.Text`
+  font-family: "Quicksand_700Bold";
+  color: #e6e6e6;
+  font-size: 19px;
+  margin-bottom: 20px;
+`
+
+export const TaskContainer = styled.View`
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+`
+
+export const Task = styled.Text`
+  font-size: 18px;
+  font-family: "Quicksand_500Medium";
+  color: #e6e6e6;
+  width: 80%;
+`
+
+export const IconTouch = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.6
+})``;
+
+export const RemoveTaskIcon = styled(AntDesign).attrs({
+  color: "#e6e6e6",
+  size: 30,
+})``;
+
+export const Loading = styled(ActivityIndicator).attrs({
+  color: "#8c00ff",
+  size: "large"
+})``;
